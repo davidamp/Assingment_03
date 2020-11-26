51aa56813e2fdbbbec5328b17c60d46086bf4446
Cmlorent <74829904+Cmlorent@users.noreply.github.com>
2020-11-26 11:14:12 -0500

    Update tools.h

--- tools.h
+++ tools.h
@@ -1,1 +1,12 @@
+This is a tools.h
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include  <stdio.h>
+#include  <math.h>
+#include  <stdlib.h>
 
+double **Metgen(int fil, int cold);
+int JacobiMethod(int n, double **A, double *b, int *xi, double eps, int MaxIt);
+double *Mat_Vec_Mult(int m, double **mot, double *arr);
+
+#endif

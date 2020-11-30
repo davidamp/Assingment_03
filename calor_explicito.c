9cedc7d3957484c3766aa6555984e9e3a8b03f80
frankandres98 <74796572+frankandres98@users.noreply.github.com>
2020-11-30 17:15:28 -0500

    Update calor_explicito.c

--- calor_explicito.c
+++ calor_explicito.c
@@ -14,26 +14,26 @@ int main(){
 	double *vector_x, *vector_t;
 	double **U;
 	
-	/************************** Discretización    ****************************** */
-	dx=0.01;   // Discretización en el espacio
+	/************************** DiscretizaciÃ³n    ****************************** */
+	dx=0.01;   // DiscretizaciÃ³n en el espacio
 	n=1/dx;	 // n+1 es la cantidad de elementos en el array x
 	
 	
-	//para convergencia debe darse que dt/dx^2 <=1/2 (en el explícito)
+	//para convergencia debe darse que dt/dx^2 <=1/2 (en el explÃ­cito)
 	//dt/(0.01)^2 <=0.5 ----> dt<=0.00005 
 	
-	dt=0.00005; //tenemos que poner números menores que 0.00005
+	dt=0.00005; //tenemos que poner nÃºmeros menores que 0.00005
 	m=1/dt;
 
-	/****************Espacio para los vectores que tienen la discretización de x y t********************/
-	vector_x = (double*)malloc((n+1)*sizeof(double)); //Asignar el tamaño del vector
-	vector_t = (double*)malloc((m+1)*sizeof(double)); //Asignar el tamaño del vector
+	/****************Espacio para los vectores que tienen la discretizaciÃ³n de x y t********************/
+	vector_x = (double*)malloc((n+1)*sizeof(double)); //Asignar el tamaÃ±o del vector
+	vector_t = (double*)malloc((m+1)*sizeof(double)); //Asignar el tamaÃ±o del vector
 	
 	//LLenamos los vectores X y T, discretizando el espacio con su respectivo dx y dt
 	arreglo(vector_x,dx);
 	arreglo(vector_t,dt);
 	
-	//Espacio en memoria para la matriz U, la cual tendrá la solución numérica
+	//Espacio en memoria para la matriz U, la cual tendrÃ¡ la soluciÃ³n numÃ©rica
 	U=(double**)calloc((n+1),sizeof(double*));
 	if(U==NULL){
 		printf("No se ha podido reservar espacio de memoria.\n");
@@ -49,19 +49,19 @@ int main(){
 	}
 	
 	
-	//Condición inicial
+	//CondiciÃ³n inicial
 	double x;
 	for(i=0;i<n+1;i++){
 		x=vector_x[i];
-		U[i][0]=exp(x); //Condición inicial es U_0(x)=exp(x)
+		U[i][0]=exp(x); //CondiciÃ³n inicial es U_0(x)=exp(x)
 		//printf("U[%i][0]=%f\n ",i,U[i][0]);
 	}
 	
-	//Condición de estabilidad para el método explícito
-	nu=dt/(dx*dx);	//En el método explícito se nos pide que nu<=1/2 para que haya estabilidad
+	//CondiciÃ³n de estabilidad para el mÃ©todo explÃ­cito
+	nu=dt/(dx*dx);	//En el mÃ©todo explÃ­cito se nos pide que nu<=1/2 para que haya estabilidad
 	
 	
-	//LLenado de la matriz solución siguiendo el esquema numérico de diferencias finitas explícito
+	//LLenado de la matriz soluciÃ³n siguiendo el esquema numÃ©rico de diferencias finitas explÃ­cito
 	for(j=0;j<m;j++){
 		
    		for(i=1;i<n;i++){
@@ -70,18 +70,18 @@ int main(){
    	}
 	
 	
-	/***************** Generamos el archivo datos_matriz en donde guardaremos la información para graficar *****************/
+	/***************** Generamos el archivo datos_matriz en donde guardaremos la informaciÃ³n para graficar *****************/
 	FILE *fp= fopen ("datos_matriz.txt","w");
 	if (fp == NULL){
     	printf("Error opening file!\n");
     	exit(1);
 	}
 	
-	//Guardamos la información que queramos graficar
-	int tiempo=0.1;  //Para ver la solución numérica en t=0.1
+	//Guardamos la informaciÃ³n que queramos graficar
+	int tiempo=0.1;  //Para ver la soluciÃ³n numÃ©rica en t=0.1
 	int columna_graficar=20000*0.1; //debemos colocarnos en esta columna
    	for(i = 0; i <n+1;i++){
-    	fprintf (fp,"%f %f",vector_x[i], U[i][columna_graficar]); //guardamos en el archivo tanto la discretización x, como la solución numérica...
+    	fprintf (fp,"%f %f",vector_x[i], U[i][columna_graficar]); //guardamos en el archivo tanto la discretizaciÃ³n x, como la soluciÃ³n numÃ©rica...
     	fprintf(fp,"\n");                                         //...para un tiempo dado
    	}
  
@@ -97,15 +97,15 @@ void arreglo(double *vector, double dx){
 	int i;
 	for(i=0;i<n+1;i++){
 		*(vector+i)=i*dx; //es equivalente a poner vector[i] 
-		printf("%f  ",*(vector+i));
+		//printf("%f  ",*(vector+i));
 	}
-	printf("\n");
+	//printf("\n");
 }
 
 int q(double x, double t){
 	double aux;
 
-	aux=cos(atan(1)*4*t)*sin(2*atan(1)*4*x);   //función q(x,t) propuesta en el deber
+	aux=cos(atan(1)*4*t)*sin(2*atan(1)*4*x);   //funciÃ³n q(x,t) propuesta en el deber
 	return aux;
 }
 
